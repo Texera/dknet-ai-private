575b5708d269d8870256f90f7bf3d397a598eeef
Xiaozhen Liu <xiaozl3@uci.edu>
2026-01-12 19:27:19 -0800

    feat(cache): refactor amber client-side services and cache upsert logic.

--- amber/src/main/scala/org/apache/texera/amber/engine/architecture/controller/promisehandlers/PortCompletedHandler.scala
+++ amber/src/main/scala/org/apache/texera/amber/engine/architecture/controller/promisehandlers/PortCompletedHandler.scala
@@ -20,15 +20,14 @@
 package org.apache.texera.amber.engine.architecture.controller.promisehandlers
 
 import com.twitter.util.Future
-import org.apache.amber.core.WorkflowRuntimeException
-import org.apache.amber.core.storage.DocumentFactory
-import org.apache.amber.core.workflow.GlobalPortIdentity
-import org.apache.amber.core.workflow.cache.FingerprintUtil
-import org.apache.amber.engine.architecture.controller.{ControllerAsyncRPCHandlerInitializer, FatalError}
-import org.apache.amber.engine.architecture.rpc.controlcommands.{AsyncRPCContext, PortCompletedRequest, QueryStatisticsRequest}
-import org.apache.amber.engine.architecture.rpc.controlreturns.EmptyReturn
-import org.apache.amber.engine.common.virtualidentity.util.CONTROLLER
-import org.apache.amber.util.VirtualIdentityUtils
+import org.apache.texera.amber.core.WorkflowRuntimeException
+import org.apache.texera.amber.core.storage.DocumentFactory
+import org.apache.texera.amber.core.workflow.GlobalPortIdentity
+import org.apache.texera.amber.engine.architecture.controller.{ControllerAsyncRPCHandlerInitializer, FatalError, PortMaterialized}
+import org.apache.texera.amber.engine.architecture.rpc.controlcommands.{AsyncRPCContext, PortCompletedRequest, QueryStatisticsRequest}
+import org.apache.texera.amber.engine.architecture.rpc.controlreturns.EmptyReturn
+import org.apache.texera.amber.engine.common.virtualidentity.util.CONTROLLER
+import org.apache.texera.amber.util.VirtualIdentityUtils
 import org.apache.texera.web.resource.dashboard.user.workflow.WorkflowExecutionsResource
 
 /** Notify the completion of a port:
@@ -76,32 +75,21 @@ trait PortCompletedHandler {
               else operatorExecution.isOutputPortCompleted(msg.portId)
 
             if (isPortCompleted) {
-              // If this is an output port and materialized, persist cache metadata.
+              // If this is an output port and materialized, notify client (for cache upsert).
               if (!msg.input) {
                 val storageUriOpt =
                   WorkflowExecutionsResource.getResultUriByPhysicalPortId(
                     cp.workflowContext.executionId,
                     globalPortId
                   )
-                (storageUriOpt, Option(cp.workflowScheduler.physicalPlan)) match {
-                  case (Some(uri), Some(plan)) =>
-                    val fingerprint = FingerprintUtil.computeSubdagFingerprint(plan, globalPortId)
-                    val tupleCount =
-                      try {
-                        Some(DocumentFactory.openDocument(uri)._1.getCount)
-                      } catch {
-                        case _: Throwable => None
-                      }
-                    WorkflowExecutionsResource.upsertOperatorPortCache(
-                      cp.workflowContext.workflowId,
-                      globalPortId,
-                      fingerprint.subdagHash,
-                      fingerprint.fingerprintJson,
-                      uri,
-                      tupleCount,
-                      Some(cp.workflowContext.executionId)
-                    )
-                  case _ => // no-op if plan or URI is missing
+                storageUriOpt.foreach { uri =>
+                  val tupleCount =
+                    try {
+                      Some(DocumentFactory.openDocument(uri)._1.getCount)
+                    } catch {
+                      case _: Throwable => None
+                    }
+                  sendToClient(PortMaterialized(globalPortId, uri, tupleCount))
                 }
               }
 
